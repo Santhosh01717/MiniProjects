e511d2095e17b5f58be1afc362ada49f60468ad2
Santhosh <sureshsanthosh017@gmail.com>
2025-01-09 15:28:52 +0530

    Project-5: Dice game! Completed

--- project-5/project-5/src/components/Die.jsx
+++ project-5/project-5/src/components/Die.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
 const Die = (props) =>{
-    const [die, setDie] = useState(props.data);
-    const [btnStyle, SetBtnStyle] = useState({});
-    const handleChange = () =>{
-        setDie((prev) => (
-            { 
-                ...prev,
-                dieState: true
-            }
-        ))
-        SetBtnStyle({color : "white" , backgroundColor: "green"})
+    // const [die, setDie] = useState(props.data);
+    
+    const handleClick = () => {
+        props.hold(props.id);
     }
     
+    // const [btnStyle, SetBtnStyle] = useState({});
+    const styles = props.dieStatus ?   {
+              backgroundColor: "#59E391",
+              color: "white",
+              border: "none",
+              borderRadius: "5px"
+    } : {};
     return (
         <>
-            <button style={btnStyle } onClick={handleChange}>
-                {die.value}
+            <button style={styles} onClick={handleClick}>
+                {props.data}
             </button>
             
         </>
--- project-5/project-5/src/components/Tenzies.jsx
+++ project-5/project-5/src/components/Tenzies.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "../css/tenzies.css";
 import Die from "./Die";
 import { useEffect } from "react";
-import { nanoid } from "nanoid"
+import { nanoid } from "nanoid";
+import { use } from "react";
 const Tenzies = () => {
   const [num, setNum] = useState(getRandomNumber());
-  const [roll, setRoll] = useState(false);
+  const btnRef = useRef(null);
+  function isSame(){
+    let check = num[0].value;
+    return num.every(obj => obj.value == check);
+  }
+  const filled = num.every((obj) => obj.dieStatus == true );
+  const gameWon = filled && isSame();
+  const gameLoss = filled && !isSame();
   function getRandomNumber() {
     return new Array(10).fill(0).map(() => ({
       value: Math.floor(Math.random() * 6),
@@ -14,25 +22,54 @@ const Tenzies = () => {
     }));
   }
   const handleRoll = () => {
-    setRoll(!roll);
+    if(!gameWon && !gameLoss){
+      setNum((oldDice) => {
+        const newDice = oldDice.map((die) =>
+          die.dieStatus === true
+            ? die
+            : { ...die, value: Math.floor(Math.random() * 6) }
+        );
+  
+        return newDice;
+      });
+    }
+    else if(gameWon || gameLoss){
+      setNum(getRandomNumber());
+    }
   };
-  useEffect(() => {
-    const data = getRandomNumber();
-    setNum(data);
-  }, [roll]);
-  console.log(getRandomNumber());
 
+  function hold(id) {
+    // console.log(id);
+    setNum((old) => {
+      return old.map((die) => {
+        return die.id == id ? { ...die, dieStatus: !die.dieStatus } : die;
+      });
+    });
+  }
+  console.log(num);
+   useEffect(() =>{
+      btnRef.current.focus();
+   },[gameWon, gameLoss])
   return (
     <>
       <main className="main">
+        {
+          filled ? (<h1>{gameWon ? "Congratulations! You Won the Game" : "You Lost the Game"}</h1>) :  (<h1></h1>)
+        }
         <div className="dice-container">
           {num.map((obj) => (
-            <Die key={obj.id} data={obj} />
+            <Die
+              key={obj.id}
+              data={obj.value}
+              dieStatus={obj.dieStatus}
+              id={obj.id}
+              hold={hold}
+            />
           ))}
         </div>
         <div>
-          <button onClick={handleRoll} className="roll-btn">
-            Roll
+          <button ref={btnRef} onClick={handleRoll} className="roll-btn">
+            {(gameWon || gameLoss) ? "New Game" : "Roll"}
           </button>
         </div>
       </main>
